686106cbe6625c8aa0965e358339b654af96a8f6
Fabrice ManzO <36665287+ziomanzo@users.noreply.github.com>
2018-07-22 11:51:32 +0200

    Add files via upload
    Functions (setters and getters) + state variables in Solidity

---
+++ functions_and_StateVariables.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.0;
+
+
+contract Person{     
+    string name;     //state variable name
+    uint age;        // state variable age
+                     //uint is Unsigned integer, only positive numbers
+
+
+    function getName() view returns (string) {   	 //getter with no input and a string as output.
+        return name;  //return string name  		 //get the value of name and display
+    }
+
+    function getAge() view returns(uint){  		 //getter with no input and a Uint as output
+        return age;                         
+    }
+    function setName(string _name) returns (string) {    //function setter to set user input for age
+        name = _name;          				 // user input _name set to state variable name
+    }
+
+    function setAge(uint _age){  			 //function setter to set user input _age  to state variable age
+        age = _age;
+    }
+}
+
+//by Fabrice Manzo,   teachers: Ivan Liljeqvist and Filip Martinsson
